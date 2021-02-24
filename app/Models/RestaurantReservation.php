8832e333853448a4e8285f56f7e49332cb9f7584
Sander Jochems <sanderjochems@hotmail.nl>
2021-02-24 11:18:49 +0100

    Update Restaurant Models

--- app/Models/RestaurantReservation.php
+++ app/Models/RestaurantReservation.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class RestaurantReservation extends Model
 {
     use HasFactory;
+
+    public function restaurant()
+    {
+        return $this->hasOne(Restaurant::class);
+    }
 }
