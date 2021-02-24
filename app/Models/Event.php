81278ca078846337fe3100ee9c491b590854ada5
Sander Jochems <sanderjochems@hotmail.nl>
2021-02-24 10:32:23 +0100

    Update IDEA

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'start_datetime' => 'date',
+        'end_datetime' => 'date'
+    ];
+
+    /**
+     * Get the reservations for the event.
+     */
+    public function reservations()
+    {
+        return $this->hasMany(EventReservation::class);
+    }
 }
