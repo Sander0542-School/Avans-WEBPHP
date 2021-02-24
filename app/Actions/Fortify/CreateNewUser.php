87789cf26aeaa263d2431a686dc4317e6a5cf113
Sander Jochems <sanderjochems@hotmail.nl>
2021-02-24 13:56:25 +0100

    Update Form

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -25,12 +25,24 @@ class CreateNewUser implements CreatesNewUsers
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+            'country' => ['required', 'string', 'max:255'],
+            'state' => ['required', 'string', 'max:255'],
+            'city' => ['required', 'string', 'max:255'],
+            'zip_code' => ['required', 'string', 'max:255'],
+            'street' => ['required', 'string', 'max:255'],
+            'building_number' => ['required', 'string', 'max:255'],
         ])->validate();
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'country' => $input['country'],
+            'state' => $input['state'],
+            'city' => $input['city'],
+            'zip_code' => $input['zip_code'],
+            'street' => $input['street'],
+            'building_number' => $input['building_number']
         ]);
     }
 }
